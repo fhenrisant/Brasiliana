9f948547807a4371a6d38f58c99d57315c92ddff
fabio henrique <fabio.santos@serpro.gov.br>
2021-02-22 17:27:30 -0300

    versão 1.0.20

--- javascript/encomendas.js
+++ javascript/encomendas.js
@@ -1,30 +1,69 @@
+(()=>{ 
+
 const botaoEncomendaPao = document.querySelector(".cartao__botao--pao");
-let contadorPao = 0
 const listaEncomenda = document.querySelector(".lista__encomenda");
 const itemEncomenda = document.createElement("li");
 itemEncomenda.classList.add("item__encomenda");
-botaoEncomendaPao.addEventListener ("click",(evento)=>{
-    contadorPao++;
-    evento.preventDefault;
+
+const CriarEncomenda = ()=>{
     
+    let contadorPao = 0
     
-    if (contadorPao < "2") {
-        let conteudo = `${contadorPao}Pão`;
+    botaoEncomendaPao.addEventListener ("click",(evento)=>{
+   
+        contadorPao++;
+    
+        if (contadorPao < "2") {
+        let conteudo = `${contadorPao} Pão`;
         itemEncomenda.innerHTML = conteudo;
         listaEncomenda.appendChild(itemEncomenda);
-    }else{
-        conteudo = `${contadorPao}Pães`;
+       
+        }else{
+        conteudo = `${contadorPao} Pães`;
         itemEncomenda.textContent = conteudo;
+        }
+
+        itemEncomenda.appendChild(BotaoConfirmar());
+        itemEncomenda.appendChild(BotaoExcluir());
+    })
+
+}
+
+const ConfirmarEncomenda = (evento) => {
+    const botaoConfirmado = evento.target;
+    const itemConfirmado = botaoConfirmado.parentElement;
+    itemConfirmado.classList.add("item__confirmado");
+
+}
+
+
+const BotaoConfirmar = ()=>{
+    const botaoConfirmar = document.createElement("button");   
+    botaoConfirmar.classList.add("item__confirmar");
+    botaoConfirmar.innerText = "Confirmar";     
+    botaoConfirmar.addEventListener("click",ConfirmarEncomenda);
+
+    return botaoConfirmar;
+}
+
+CriarEncomenda();
+
+const BotaoExcluir = ()=>{
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.innerText = "Excluir";
+    botaoExcluir.addEventListener("click",ExcluirItem)
+
+    return botaoExcluir;
+}
+
+const ExcluirItem = (evento)=>{
+    const botaoExcluido = evento.target;
+    const itemExcluido = botaoExcluido.parentElement;
+    itemExcluido.remove();
+    CriarEncomenda();
+    itemEncomenda.classList.remove("item__confirmado");
+
+    return botaoExcluido;
+        
     }
-   
-});
-
-const botaoEncomendaQueijo = document.querySelector(".cartao__botao--queijo");
-botaoEncomendaQueijo.addEventListener ("click",(evento)=>{
-    evento.preventDefault;
-    console.log("Encomenda de Queijo")});
-
-const botaoEncomendaTorta = document.querySelector(".cartao__botao--torta");
-botaoEncomendaTorta.addEventListener ("click",(evento)=>{
-    evento.preventDefault;
-    console.log("Encomenda de Torta")});
+})()
